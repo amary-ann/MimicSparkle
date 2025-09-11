3ee6cb143de0dd70cb8cdfde8ec7252b5f788e0c
amary-ann <marynaj28@gmail.com>
2025-09-11 03:51:55 +0100

    commit

--- main.py
+++ main.py
@@ -451,7 +451,7 @@ async def whatsapp_callback(request: Request):
 
                     elif msg.get("type") == "audio":
                         media_content_type = msg.get("audio",{}).get("mime_type", "")
-                        file = await save_media_to_file(media_content_type, msg.get("image",{}).get("id", None))
+                        file = await save_media_to_file(media_content_type, msg.get("audio",{}).get("id", None))
                         message_body = process_audio_bytes(file)
                         # msg_content = f"Audio Message content: {msg}"
                         print(f"{message_body=}")
